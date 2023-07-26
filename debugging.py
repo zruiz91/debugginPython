0854b6b2396b3a92f51bb87ddea39fffacf50f9c
Zach Ruiz <powersbooth10@gmail.com>
2023-07-26 11:35:54 -0500

    solved debug lesson 3.

--- debugging.py
+++ debugging.py
@@ -1,28 +1,31 @@
 ############DEBUGGING#####################
 
 # # Describe Problem
-# the loop uses a range function that takes two numbers , the start and the end (excluding the end number.)
-# That' why the print statement never triggers because i never equals 20 since the end number isnt included in the range
-# solve by changing the end number in range to 21
+# # the loop uses a range function that takes two numbers , the start and the end (excluding the end number.)
+# # That' why the print statement never triggers because i never equals 20 since the end number isnt included in the range
+# # solve by changing the end number in range to 21
 # def my_function():
 #   for i in range(1, 20):
 #     if i == 20:
 #       print("You got it")
 # my_function()
 
-# Reproduce the Bug
-from random import randint
-dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(0, 5)
-print(dice_num)
-print(dice_imgs[dice_num])
+# # Reproduce the Bug
+# # the problem is that the randint function is being used with 1 and 6 as the range, which then gets used to access an element from the array of images
+# # the solution is changing the range to 0 and 5 so that die 1 gets shown and so that it doesnt error out trying to acces something out of the range
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(0, 5)
+# print(dice_imgs[dice_num])
 
 # # Play Computer
-# year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
-#   print("You are a millenial.")
-# elif year > 1994:
-#   print("You are a Gen Z.")
+# # The issue is that if comparison only acounts for the years in between 1980 and 1994 with outh including those numbers
+# # The solution to the issue is to change < and > to  >= and <= in the if comparison 
+year = int(input("What's your year of birth?"))
+if year >= 1980 and year <= 1994:
+  print("You are a millenial.")
+elif year > 1994:
+  print("You are a Gen Z.")
 
 # # Fix the Errors
 # age = input("How old are you?")
